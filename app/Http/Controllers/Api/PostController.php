e322d306889b277780a5151e03de229994c625d7
GiuliaSardell <giuliasardelli1234@gmail.com>
2022-02-09 15:59:42 +0100

    rotta api e postcontroller

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+
+        $posts = Post::all(); //prendo tutti i post
+        return response()->json(compact('posts'));
+    }
+}
