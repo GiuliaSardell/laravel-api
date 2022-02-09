5d6715a7f19b26256feee33de542ac3c17a3be50
GiuliaSardell <giuliasardelli1234@gmail.com>
2022-02-09 17:37:38 +0100

    header nav

--- resources/js/components/partials/Header.vue
+++ resources/js/components/partials/Header.vue
@@ -1,6 +1,21 @@
 <template>
   <header>
-    <nav>NAV</nav>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a class="active" href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Chi siamo</a>
+        </li>
+        <li>
+          <a href="#">Contatti</a>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -11,5 +26,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+header{
+  background-color: burlywood;
+  text-align: center;
+  padding: 20px 0;
+  ul{
+    list-style: none;
+    li{
+      display: inline-block;
+      margin: 0 15px;
+      a{
+        color:white;
+        text-decoration: none;
+        &:hover{
+          text-decoration: underline;
+        }
+        &.active{
+          color: brown;
+        }
+      }
+    }
+  }
+}
 </style>
